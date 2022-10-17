f7acfec3d4e08f825d423a0032a6f101e56c204c
virginia-oc <94895896+virginia-oc@users.noreply.github.com>
2022-10-17 18:32:58 +0200

    Ex.3.11.1 done

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,15 +8,17 @@ public class GameController : MonoBehaviour
     private int puntos;
     private int vidas;
     private int itemsRestantes;
+    private int nivelActual;
     [SerializeField] TMPro.TextMeshProUGUI HUD;
     [SerializeField] Transform prefabKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        puntos = 0;
-        vidas = 3;
         itemsRestantes = FindObjectsOfType<Coin>().Length;
+        puntos = FindObjectOfType<GameStatus>().puntos;
+        vidas = FindObjectOfType<GameStatus>().vidas;
+        nivelActual = FindObjectOfType<GameStatus>().nivelActual;
         HUD.text = "Puntos: " + puntos + "\n" +
             "Vidas: " + vidas;
     }
@@ -30,7 +32,9 @@ public class GameController : MonoBehaviour
     public void AnotarItemRecogido()
     {
         puntos += 10;
+        FindObjectOfType<GameStatus>().puntos = puntos;
         itemsRestantes--;
+        
         if (itemsRestantes <= 0)
         {
             Vector3 position = new Vector3(6.11f, -1.82f, 0f);
@@ -44,6 +48,8 @@ public class GameController : MonoBehaviour
     public void PerderVida()
     {
         vidas--;
+        FindObjectOfType<GameStatus>().vidas = vidas;
+
         HUD.text = "Puntos: " + puntos + "\n" +
             "Vidas: " + vidas;
         FindObjectOfType<Player>().SendMessage("ResetPosition");
@@ -56,6 +62,14 @@ public class GameController : MonoBehaviour
 
     private void AvanzarNivel()
     {
-        SceneManager.LoadScene("Level2");
+        nivelActual++;
+
+        if (nivelActual > FindObjectOfType<GameStatus>().nivelMasAlto)
+        {
+            nivelActual = 1;
+        }
+
+        FindObjectOfType<GameStatus>().nivelActual = nivelActual;
+        SceneManager.LoadScene("Level"+nivelActual);
     }
 }

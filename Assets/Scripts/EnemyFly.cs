b46e7b0f2300d319960f65bedcdb40eed40824c4
virginia-oc <94895896+virginia-oc@users.noreply.github.com>
2022-10-17 11:16:44 +0200

    Ex.3.9.1 done

--- Assets/Scripts/EnemyFly.cs
+++ Assets/Scripts/EnemyFly.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class EnemyFly : MonoBehaviour
 {
+    [SerializeField] List<Transform> wayPoints;
+    private float velocidad;
+    private Vector3 siguientePosicion;
+    private byte siguienteIndice = 0;
+    private float distanciaCambio = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocidad = 2;
+        siguientePosicion = wayPoints[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(
+            transform.position, 
+            siguientePosicion,
+            velocidad * Time.deltaTime);
+
+        if (Vector3.Distance(
+            transform.position, siguientePosicion) < distanciaCambio)
+        {
+            siguienteIndice++;
+
+            if (siguienteIndice >= wayPoints.Count)           
+                siguienteIndice = 0;
+            
+            siguientePosicion = wayPoints[siguienteIndice].position;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)

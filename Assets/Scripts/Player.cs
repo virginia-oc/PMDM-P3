2722250c10f433ccdd607e9aaf12688726b1c789
virginia-oc <94895896+virginia-oc@users.noreply.github.com>
2022-10-20 10:32:52 +0200

    Ex.3.14.1 done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float velocidad = 2.0f;
-    [SerializeField] float velocidadSalto = 30.0f;
+    [SerializeField] float velocidadSalto = 2.0f;
     private float xInicial, yInicial;
     private float alturaPlayer;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -15,29 +16,46 @@ public class Player : MonoBehaviour
         xInicial = transform.position.x;
         yInicial = transform.position.y;
         alturaPlayer = GetComponent<Collider2D>().bounds.size.y;
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(
+                transform.position, new Vector2(0, -1));
+        float distanciaSuelo = hit.distance;
+        bool tocandoSuelo = distanciaSuelo < alturaPlayer;
         float horizontal = Input.GetAxis("Horizontal");
+
+        if (tocandoSuelo && (horizontal > 0.1f))
+        {
+            anim.Play("PersonajeCorriendoDerecha");
+        }
+        else if (tocandoSuelo && (horizontal < -0.1f))
+        {
+            anim.Play("PersonajeCorriendoIzquierda");
+        }
+ 
         transform.Translate(horizontal * velocidad * Time.deltaTime, 0, 0);
 
         float salto = Input.GetAxis("Jump");
-        
+
         if (salto > 0)
         {
-            RaycastHit2D hit = Physics2D.Raycast(
-                transform.position, new Vector2(0, -1));
-            float distanciaSuelo = hit.distance;
-            
-            bool tocandoSuelo = distanciaSuelo < alturaPlayer * 4/6;
-
             if (tocandoSuelo)
             {
                 Vector3 fuerzaSalto = new Vector3(0, velocidadSalto, 0);
                 GetComponent<Rigidbody2D>().AddForce(fuerzaSalto);
             }
+        }
+
+        if (tocandoSuelo == false)
+        {
+            if (horizontal < 0)
+                anim.Play("PersonajeSaltandoIzquierda");
+            else
+                anim.Play("PersonajeSaltando");
         }
     }
 
